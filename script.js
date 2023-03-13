a0ac236a804924e646d7a2a740154f52b7d6ee38
colecrum26 <cwcrumrine@gmail.com>
2023-03-13 10:19:41 -0500

    working with switch case

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-let str1 = "lasagna";
-console.log();
+// let str1 = "lasagna";
+// console.log();
 
-let arr1 = [1,2,3,4,5,"itsa me, chris pratt, the poser mario"]
-// console.log(arr1[5]);
+// let arr1 = [1,2,3,4,5,"itsa me, chris pratt, the poser mario"]
+// // console.log(arr1[5]);
 
-if (arr1.indexOf("pizza")===-1) {
-  console.log("this is not present in the array");
-}
-else{
-    console.log("it's present")
+// if (arr1.indexOf("pizza")===-1) {
+//   console.log("this is not present in the array");
+// }
+// else{
+//     console.log("it's present")
+// }
+
+let path = "bad";
+
+switch (path) {
+  case "good":
+    console.log("you have made a good choice");
+    break;
+  case "bad":
+    console.log("BAD, WRONG!");
+    break;
+  default:
+    console.log("if you choose not to decide, you still have made a choice");
+    break;
 }
